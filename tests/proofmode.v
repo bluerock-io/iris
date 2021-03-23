7da8952e3cb5783e9dc91ea3abc60fa2706d08a4
Ralf Jung <jung@mpi-sws.org>
2021-03-23 19:28:58 +0100

    add more tests

--- tests/proofmode.v
+++ tests/proofmode.v
@@ -457,17 +457,17 @@ Proof. iIntros (?) "HP #HQ HR". iPureIntro; eauto. Qed.
 
 (* Ensure that [% ...] works as a pattern when the left-hand side of and/sep is
 pure. *)
-Lemma test_pure_and_sep_destruct_affine `{!BiAffine PROP} (φ : Prop) (P : PROP) :
+Lemma test_pure_and_sep_destruct_affine `{!BiAffine PROP} (φ : Prop) P :
   ⌜φ⌝ ∧ (⌜φ⌝ ∗ P) -∗ P.
 Proof.
   iIntros "[% [% $]]".
 Qed.
-Lemma test_pure_and_sep_destruct_1 (φ : Prop) (P : PROP) :
+Lemma test_pure_and_sep_destruct_1 (φ : Prop) P :
   ⌜φ⌝ ∧ (<affine> ⌜φ⌝ ∗ P) -∗ P.
 Proof.
   iIntros "[% [% $]]".
 Qed.
-Lemma test_pure_and_sep_destruct_2 (φ : Prop) (P : PROP) :
+Lemma test_pure_and_sep_destruct_2 (φ : Prop) P :
   ⌜φ⌝ ∧ (⌜φ⌝ ∗ <absorb> P) -∗ <absorb> P.
 Proof.
   iIntros "[% [% $]]".
