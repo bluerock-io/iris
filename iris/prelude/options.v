e69b5f0a036648e997d9ce36b8b4fe277a542e8f
Ralf Jung <jung@mpi-sws.org>
2023-09-14 14:29:07 +0200

    deal with some Coq 8.18 warnings

--- iris/prelude/options.v
+++ iris/prelude/options.v
@@ -10,11 +10,11 @@ file is *imported*, the option will only apply on the import site
 but not transitively. *)
 From stdpp Require Export options.
 
-Export Set Suggest Proof Using. (* also warns about forgotten [Proof.] *)
+#[export] Set Suggest Proof Using. (* also warns about forgotten [Proof.] *)
 
 (* We always annotate hints with locality ([Global] or [Local]). This enforces
 that at least global hints are annotated. *)
-Export Set Warnings "+deprecated-hint-without-locality".
+#[export] Set Warnings "+deprecated-hint-without-locality".
 
 (* "Fake" import to whitelist this file for the check that ensures we import
 this file everywhere.
